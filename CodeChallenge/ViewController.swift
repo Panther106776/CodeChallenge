db1ee073141acb10dabb006ea3b23020433b446d
GERARDO FLORES <rl106776@rlas-macair-15.local>
2018-12-12 10:41:29 -0600

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by GERARDO FLORES on 9/4/18.
+//  Copyright © 2018 GERARDO FLORES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var WhatcoloramI: UILabel!
+    }
+func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+func CodeChallenge(_ sender: Any) {
+        self.view.backgroundColor = .green
+    }
+    
+    
+
+
+
